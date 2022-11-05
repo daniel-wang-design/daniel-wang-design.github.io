424b5908a1165ced56ce77a4cf7540ce0f23f147
daniel-wang-design <daniel.wang04@hotmail.com>
2022-11-05 13:09:23 -0400

    updated up arrow

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import { HiOutlineChevronDoubleUp } from "react-icons/hi";
+import { BsChevronUp } from "react-icons/bs";
 
 export const Footer = () => {
   return (
-    <div id="contact" className="w-full lg:h-screen">
+    <div id="contact" className="w-full pb-8">
       <div className="max-w-[1240px] m-auto  w-full ">
         <div className="flex justify-center ">
           <Link href="/">
             <div className="bg-[#eef0f1] p-4 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
-              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+              <BsChevronUp className="text-[#5651e5]" size={30} />
             </div>
           </Link>
         </div>
--- pages/Project1.jsx
+++ pages/Project1.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import propertyImg from '../public/assets/projects/property.jpg';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
 
 const Project1 = () => {
   return (
-    <div>Project1</div>
-  )
-}
+    <div className='w-full'>
+     
+    </div>
+  );
+};
 
-export default Project1
+export default Project1;
