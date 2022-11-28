f37a5e2892bd6e17a0ea63852b204f4626b1913c
daniel-wang-design <daniel-wang04@hotmail.com>
2022-11-27 20:03:02 -0500

    update

--- pages/Improvall.jsx
+++ pages/Improvall.jsx
@@ -85,7 +85,7 @@ const Improvall = () => {
       </div>
       <div className="p-8 max-w-[1240px] w-full h-auto pt-2 m-auto">
         <h1 className="text-[#bb86fc] tracking-wide">PRODUCT SHOWCASE</h1>
-        <div className="gap-2 pt-10 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+        <div className="gap-2 pt-10 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
           <Image
             className="rounded-xl object-fill w-[100vw]"
             src={improvall2}
