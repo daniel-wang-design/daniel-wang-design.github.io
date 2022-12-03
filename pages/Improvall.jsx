7d59d78aca776c085d4bbb8c083e5fb3eb442afd
daniel-wang-design <daniel-wang04@hotmail.com>
2022-12-03 16:42:17 -0500

    update

--- pages/Improvall.jsx
+++ pages/Improvall.jsx
@@ -27,11 +27,11 @@ const Improvall = () => {
         </div>
         <div
           className="absolute top-[10vh] left-[10vw] lg:top-[30%]
-      lg:left-[10%] sm:top-[10vh] sm:left-[10%] md:top-[30vh] md:left-[10%]"
+      lg:left-[10%] sm:top-[10vh] sm:left-[10%] md:top-[30vh] md:left-[10%] max-w-[1240px]"
         >
           <TypeAnimation
             sequence={["IMPROVALL", 2000]}
-            className="text-[10vw] py-4 tracking-widest text-[#bb86fc] max-w-[1240px]"
+            className="text-[10vw] py-4 tracking-widest text-[#bb86fc] "
             cursor={true}
             repeat={0}
             speed={10}
